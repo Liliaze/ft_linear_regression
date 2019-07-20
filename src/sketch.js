9b79990eff7d294756da2cf0b1e667c4c423fe09
Diana Boudy <diana.boudy@gmail.com>
2019-07-20 18:17:54 +0200

    perfected graph details

--- src/sketch.js
+++ src/sketch.js
@@ -1,34 +1,27 @@
 let table;
 let img;
 let circlesArray;
-const graphOriginX = 100;
-const graphOriginY = innerHeight - 100;
-const graphCoefX = 200;
-const graphCoefY = 25;
 const maxX = innerWidth - 150;
 const maxY = 115;
+const graphOriginX = 100;
+const graphOriginY = innerHeight - 100;
+const ecartY = graphOriginY - maxY;
+const ecartX = maxX - graphOriginX;
+const linearLineX1 = graphOriginX;
+const linearLineY1 = graphOriginY;
+let linearLineX2 = maxX;
+let linearLineY2 = maxY;
+let maxKm = 0;
+let maxPrice = 0;
+let theta0 = 0;
+let theta1 = 0;
 
 function setup() {
   //add input load file
   input = createFileInput(handleFile);
   input.position(innerWidth / 2 - 100, 95);
-
-  
   createCanvas(innerWidth, innerHeight);
-  background(150);
-  //add button load file
-  /*
-  button = createButton('load');
-  button.position(19, 19);
-  button.mousePressed(loadFile);
-  */
 }
-
-/*
-function loadFile(file) {
-  
-}
-*/
 
 function handleFile(file) {
     img = null;
@@ -45,6 +38,8 @@ function handleFile(file) {
 
 function loadTableOn() {
   circlesArray = [];
+  maxPrice = 0;
+  maxKm = 0;
   print(table.getRowCount() + ' total rows in table');
   print(table.getColumnCount() + ' total columns in table');
   print(table.getColumn('km'));
@@ -52,30 +47,49 @@ function loadTableOn() {
 
   //cycle through the table
   for (let r = 0; r < table.getRowCount(); r++){
-    circlesArray.push([table.getString(r, 'km'),table.getString(r, 'price')])
-      //print("km : " + table.getString(r, 'km'));
-      //print("price : " + table.getString(r, 'price'));
+    if (table.getString(r, 'km') > maxKm)
+      maxKm = table.getString(r, 'km');
+    if (table.getString(r, 'price') > maxPrice)
+      maxPrice = table.getString(r, 'price');
+    circlesArray.push([table.getString(r, 'km'), table.getString(r, 'price')])
   }
 }
 
 function draw() {
+  //clean
   background(255);
+  textSize(30);
   stroke('black');
   fill('blue');
-  textSize(30);
+  //display interface
   text('FT_LINEAR_REGRESSION', innerWidth / 2 - 8*28, 30);
   text('BY DBOUDY', innerWidth / 2 - 4*28, 70);
   fill('black');
   line(graphOriginX, graphOriginY, maxX, graphOriginY);
   line(graphOriginX,graphOriginY, graphOriginX, maxY);
+  //dislay file img
   if (img) {
     image(img, graphOriginX, maxY, innerWidth - 200 , innerHeight - 200);
   }
+  //dislay linear graph
   else if (table) {
+    textSize(15);
+    text('0', graphOriginX -16, graphOriginY + 16);
+    text('price\n' + maxPrice, graphOriginX - 40, maxY);
+    text('km\n' + maxKm, maxX + 10, graphOriginY);
     fill('red');
     stroke('red');
     for (let circleIndex in circlesArray) {
-      circle(circlesArray[circleIndex][0]/graphCoefX + graphOriginX, (-(circlesArray[circleIndex][1]/graphCoefY) + graphOriginY), 5);
+      let posX = (ecartX * circlesArray[circleIndex][0])/maxKm + graphOriginX;
+      let posY = graphOriginY - (ecartY * circlesArray[circleIndex][1]/maxPrice)
+      circle(posX, posY, 5);
+      textSize(11);
+      text("(" + circlesArray[circleIndex][1] + "," + circlesArray[circleIndex][0] + ")", posX + 5, posY);
     }
+    fill('green');
+    stroke('green');
+    //to do define theta0 et theta1
+    line(linearLineX1, linearLineY1, linearLineX2, linearLineY2);
   }
 }
+
